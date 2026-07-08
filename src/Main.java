5c2ffa77d547132a630bb431f6dee93d8f864acc
debora-oliv <deboraalves007@hotmail.com>
2026-07-08 09:52:45 -0300

    feat: testes das views iniciais

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import controller.DiscenteController;
-import view.TelaCadastroDiscente;
+import controller.*;
+import view.*;
 
 import javax.swing.SwingUtilities;
 
@@ -8,14 +8,50 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // Instancia a tela (carrega o visual e os componentes)
-                TelaCadastroDiscente tela = new TelaCadastroDiscente();
+                // TESTE DISCENTE
+//                TelaCadastroDiscente tela = new TelaCadastroDiscente();        // Instancia a tela (carrega o visual e os componentes)
+//                DiscenteController controller = new DiscenteController(tela);  // Instancia o Controller para amarrar eventos (cliques nos botões)
+//                tela.setVisible(true);                                         // Torna a janela visível para o usuário
 
-                // Instancia o Controller passando a tela para ele amarrar os eventos (cliques nos botões)
-                DiscenteController controller = new DiscenteController(tela);
+                // TESTE PERIODO
+//                TelaCadastroPeriodo tela = new TelaCadastroPeriodo();
+//                PeriodoController controller = new PeriodoController(tela);
+//                tela.setVisible(true);
 
-                // Torna a janela visível para o usuário
+                // TESTE PLANO ATIVIDADES
+//                TelaCadastroPlanoAtividades tela = new TelaCadastroPlanoAtividades();
+//                PlanoAtividadesController controller = new PlanoAtividadesController(tela);
+//                tela.setVisible(true);
+
+                // TESTE PLANO ATIVIDADES
+//                TelaCadastroTermoCompromisso tela = new TelaCadastroTermoCompromisso();
+//                TermoCompromissoController controller = new TermoCompromissoController(tela);
+//                tela.setVisible(true);
+
+                // TESTE COORDENADOR
+//                TelaCadastroCoordenador tela = new TelaCadastroCoordenador();
+//                CoordenadorController controller = new CoordenadorController(tela);
+//                tela.setVisible(true);
+
+                // TESTE SUPERVISOR
+//                TelaCadastroSupervisor tela = new TelaCadastroSupervisor();
+//                SupervisorController controller = new SupervisorController(tela);
+//                tela.setVisible(true);
+
+                // TESTE CONCEDENTE
+//                TelaCadastroConcedente tela = new TelaCadastroConcedente();
+//                ConcedenteController controller = new ConcedenteController(tela);
+//                tela.setVisible(true);
+
+                // TESTE ESTAGIO
+                TelaCadastroEstagio tela = new TelaCadastroEstagio();
+                EstagioController controller = new EstagioController(tela);
                 tela.setVisible(true);
+
+                // TESTE SUPERVISOR
+//                TelaCadastroAproveitamento tela = new TelaCadastroAproveitamento();
+//                AproveitamentoController controller = new AproveitamentoController(tela);
+//                tela.setVisible(true);
             }
         });
     }
